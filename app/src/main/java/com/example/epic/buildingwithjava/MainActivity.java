83a0efbae60d3931b19347ceeb3a178655da37fa
Erick Tepale <e.tepale@gmail.com>
2018-01-22 18:37:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/epic/buildingwithjava/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.epic.buildingwithjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout; //manually imported
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //using a relative layout
+        RelativeLayout epicLayout = new RelativeLayout(this);
+        epicLayout.setBackgroundColor(Color.GREEN);
+
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText(R.string.clickME);
+        redButton.setBackgroundColor(Color.RED);
+        redButton.setId(R.id.button_one); //this gets the id from xml file ids.xml
+
+        //setting button in center horizontally & vertically
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        //Username Input
+        EditText username = new EditText(this);
+        username.setId(R.id.EDITTEXTUSERNAME); //this gets the ID from the ids.xml file that was created
+
+        //setting details for username input
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+
+
+
+        //Set button to layout
+        epicLayout.addView(redButton, buttonDetails); //adds button and button details to the layout or widget
+        epicLayout.addView(username, usernameDetails);
+
+        //Set layout to display on screen
+        setContentView(epicLayout); //makes this layout the main screen, meaning that the button will display
+    }
+}
